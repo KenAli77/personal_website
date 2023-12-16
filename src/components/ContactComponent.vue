7e679ecee0fd52453ad261754f9953f86274e1af
Ken <kenediidali@gmail.com>
2023-12-16 14:46:03 +0100

    Add new route for contact page

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="contact">
+        <v-card>
+            <v-card-title class="headline">Contact Us</v-card-title>
+            <v-card-text>
+                <v-form @submit.prevent="submitForm">
+                    <v-row>
+                        <v-col cols="12" sm="6">
+                            <v-text-field v-model="name" label="Name" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-text-field v-model="email" label="Email" required></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-textarea v-model="message" label="Message" required></v-textarea>
+                    <v-btn type="submit" color="primary">Submit</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const email = ref('');
+const message = ref('');
+
+const submitForm = () => {
+    // Handle form submission logic here
+};
+</script>
+
+<style scoped>
+.contact {
+    height: 100vh;
+    background-color: #f5f5f5;
+
+}
+/* Add custom styles here */
+</style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="home">
+        <v-container fluid>
+            <v-row align="center" justify="center">
+                <v-col cols="12" sm="8" md="6">
+                    <h1 class="display-4">Welcome to My Portfolio</h1>
+                    <p class="subtitle-1">ipsem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam commodi, iste exercitationem dicta autem ducimus atque ea doloribus illum ipsa, ex alias dolorum illo, cumque minima. Voluptatibus ab similique iure!</p>
+                    <v-btn color="primary" class="mt-4">View Projects</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.home {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5f5f5;
+}
+
+.display-4 {
+    font-size: 3rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+}
+
+.subtitle-1 {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+}
+
+.mt-4 {
+    margin-top: 1rem;
+}
+</style>
